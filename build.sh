86f0100ed893e86cb74472e27cda5a6a1473b5cf
Sambram08 <162282454+Sambram08@users.noreply.github.com>
2025-03-24 00:44:24 +0530

    Normalized line endings

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
 
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# python manage.py migrate
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
+
+
+
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -o errexit  
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Apply Django migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+
+# Collect static files
+python manage.py collectstatic --noinput
